d645f361beae6c1d775d559beae4a03000584f36
oumy <toumy8712@gmail.com>
2021-11-17 09:37:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appli_jeu_capteur/CharacterSprite.java
@@ -0,0 +1,45 @@
+package com.example.appli_jeu_capteur;
+
+
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+
+
+public class CharacterSprite {
+
+
+
+    private Bitmap image;
+    public int x, y;
+    private final int xVelocity = 10;
+    public int yVelocity = 5;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+    public CharacterSprite (Bitmap bmp) {
+        image = bmp;
+        x = 100;
+        y = 100;
+    }
+
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+
+
+    }
+    public void update() {
+
+        y += yVelocity;
+
+
+
+    }
+
+    }
+
+
+
